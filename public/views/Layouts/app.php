fed0f20ac1fbdd90352070f5cce1590c85cb35b9
Nadji_Tan <nadjitan@yahoo.com.ph>
2021-01-11 14:02:57 +0800

    Layout fix

--- public/views/Layouts/app.php
+++ public/views/Layouts/app.php
@@ -9,20 +9,20 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 
     <!-- <link rel="stylesheet" href="/public/css/app.css"> -->
-</head>
-
-<body>
     <style>
         <?php 
             echo file_get_contents('./public/css/app.css');
         ?>
     </style>
+</head>
+
+<body>
     <header>
         <nav class="main-navigation-scroll">
             <input type="checkbox" id="nav-toggle" />
 
             <a href="home" class="logo" tabindex="-1"
-                ><img src="" /> LOGO
+                >LOGO<img src="" />
             </a>
 
             <div class="nav-links">
